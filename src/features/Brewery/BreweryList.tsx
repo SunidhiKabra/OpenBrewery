0ca2a1d82b609a6205f7527beb1158d77a4e520e
Sunidhi Kabra <kabra.sunidhi14.com>
2022-02-27 13:39:06 -0600

    Added styling for header

--- src/features/Brewery/BreweryList.tsx
+++ src/features/Brewery/BreweryList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { Header, Icon } from 'semantic-ui-react';
+import './BreweryList.css';
 import 'semantic-ui-css/semantic.min.css';
 
 export function BreweryList(): JSX.Element {
