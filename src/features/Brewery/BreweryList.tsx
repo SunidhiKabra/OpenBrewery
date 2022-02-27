695ce743e11b41fd1dd319798fc063d8d65eae68
Sunidhi Kabra <kabra.sunidhi14.com>
2022-02-27 13:41:17 -0600

    Show cards with breweries

--- src/features/Brewery/BreweryList.tsx
+++ src/features/Brewery/BreweryList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { Header, Icon } from 'semantic-ui-react';
+import { Card, Header, Icon } from 'semantic-ui-react';
 import './BreweryList.css';
 import 'semantic-ui-css/semantic.min.css';
 
@@ -29,6 +29,38 @@ export function BreweryList(): JSX.Element {
         <Icon name='beer' circular />
         <Header.Content>Breweries in Chicago!</Header.Content>
       </Header>
-    </div>
+      <Card.Group itemsPerRow={3}>
+        {breweries.map((brewery) => (
+          <Card key={brewery.id} color='blue' fluid>
+            <Card.Header>
+              {brewery.name}
+            </Card.Header>
+            <Card.Content>
+              <div>{brewery.type}</div>
+              <div>
+                  <p>
+                    {brewery.street ? ` ${brewery.street}` : ''}
+                    {brewery.city ? ` ${brewery.city},` : ''} 
+                  </p>
+                  <p>
+                    {brewery.state ? ` ${brewery.state}` : ''} 
+                    {brewery.postal_code ? ` ${brewery.postal_code}` : ''}
+                  </p>
+                </div>
+            </Card.Content>
+            <Card.Content>
+              <div>
+                {brewery.website_url && 
+                  <>
+                    <Icon name="mail"/>
+                    {brewery.website_url}
+                  </>
+                } 
+              </div>
+            </Card.Content>
+          </Card>
+        ))}
+        </Card.Group>
+      </div>
   );
 }
